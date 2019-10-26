9233d0e14fb5f5576655a9c341cbc9c15f116359
Zackery Bayle <Zackery.bayle@gmail.com>
2019-10-26 09:49:05 -0700

    basic list

--- Python/Math/list.py
+++ Python/Math/list.py
@@ -1,7 +1,18 @@
 
 users = ['John', 'Tiffany', 'Kyle']
+tags = ['python', 'development', 'tutorials', 'code']
 
 
 users.insert(0, 'Tony')
+num_tags = len(tags)
 
+print(users)
+print(num_tags)
+
+
+users.sort()
+
+print(users)
+
+users.sort(reverse = True)
 print(users)

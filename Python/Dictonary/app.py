d1b7fc3ea1ce4b35f988ced92f6d801cd6c02986
Zackery Bayle <Zackery.bayle@gmail.com>
2019-10-29 11:02:34 -0700

    Dictonary inital commit

---
+++ Python/Dictonary/app.py
@@ -0,0 +1,23 @@
+players = {
+    "ss": "Correa",
+    "2b": "Altuve",
+    "3b": "Bregman",
+    "dh": "Gavis",
+    "of": "Springer",
+}
+
+teams = {
+    "astros": {
+        "ss": "Correa",
+        "2b": "Altuve",
+        "3b": "Bregman",
+        "dh": "Gavis",
+        "of": "Springer",
+    }
+}
+
+teams['red sox'] = {'ss': 'Betts'}
+
+second_base = players['2b']
+# print(teams['astros']['ss'])
+print(teams['red sox'])

f7dda618ee82514cc350ed12213577eaa21411f4
Zackery Bayle <Zackery.bayle@gmail.com>
2019-10-31 11:21:09 -0700

    turpl tinkering

---
+++ Python/Math/turpl.py
@@ -0,0 +1,29 @@
+post = (
+    'Python Basics',
+    'Intro guid to python',
+    'Some kool python content here!'
+)
+post_list = [
+    'Python Basics',
+    'Intro guid to python',
+    'Some kool python content here!'
+]
+
+
+
+
+# Tuple: Immutable
+# List: Mutable
+
+# post_list.sort()
+
+
+
+post += ('published',)
+print(len(post))
+# title, sub_heading, content, status = post
+
+# print(title)
+# print(sub_heading)
+# print(content)
+# print(status)

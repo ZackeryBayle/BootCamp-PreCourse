fc2035d175d71bcfe3c73e9e8c85b944b8240965
Zackery Bayle <Zackery.bayle@gmail.com>
2019-10-31 13:39:31 -0700

    inital commit

---
+++ Python/Math/loop.py
@@ -0,0 +1,45 @@
+players = ('Altuve','Bregman', 'Correa', 'Gattis')
+
+players_dic = {
+    '2b': 'Altuve',
+    '3b': 'Bregman',
+    'ss': 'Correa',
+    'dh': 'Gattis'
+}
+
+usernames = [
+    'jon',
+    'jacob',
+    'shmitte',
+    'johnny'
+]
+
+
+alphabet = 'abdcef'
+
+# for player in players:
+#     print(player)
+
+# for key, player in players_dic.items():
+#     print(player)
+
+# for letter in alphabet:
+#     print(letter)
+
+# for num in range(0, 11):
+#     print(num)
+
+ 
+# for username in usernames:
+#     if username == 'shmitte':
+#         print(f'Sorry, {username}, you are not allowed')
+#         continue
+#     else:
+#         print(f'{username} is allowed')
+
+
+# for username in usernames:
+#     if username == 'shmitte':
+#         print(f'{username} was found at index {usernames.index(username)}')
+#         break
+#     # print(username)

28c6b00190c13b2dfd4867f8da4525500198976b
Zackery Bayle <Zackery.bayle@gmail.com>
2019-11-11 10:24:58 -0700

    comilation / conditionals inital commit

---
+++ Python/Math/conditionals.py
@@ -0,0 +1,18 @@
+# age = 66
+
+# if age < 25:
+#     print(f"I'm sorry, you need to be atleast 25 years old")
+# elif age > 65:
+#     print(f"I'm sorry, {age} is too old enough to rent a car.")
+# else:
+#     print(f"You're approved, {age} is old enough to rent a car.")
+
+
+# 
+# Ternary Operator
+# 
+
+role = 'admin'
+
+auth = 'can access' if role == 'admin' else 'cannot access'
+print(auth)
---
+++ Python/Math/listcomp.py
@@ -0,0 +1,20 @@
+num_list = range(1,11)
+
+even_numbers = []
+
+# for num in num_list:
+#     if num % 2 == 0:
+#         even_numbers.append(num)
+
+even_numbers = [num for num in num_list if num % 2 == 0]
+
+# cubed_nums = []
+
+# for num in num_list:
+#     cubed_nums.append(num ** 3)
+
+# cubed_nums = [num ** 3 for num in num_list]
+
+
+# print(cubed_nums)
+print(even_numbers)

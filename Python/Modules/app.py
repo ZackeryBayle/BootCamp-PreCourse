495288bb51c40eddf64ac710f8da1beb092efca1
Zackery Bayle <Zackery.bayle@gmail.com>
2019-11-14 08:53:01 -0700

    module import inital commit

---
+++ Python/Modules/app.py
@@ -0,0 +1,10 @@
+import sys
+from imports.helper import greeting #from outside folder
+
+# sys.path.insert(0, '../imports')
+# import imports.helper as g
+
+
+def render():
+    print(greeting('zac','bayle'))
+render()

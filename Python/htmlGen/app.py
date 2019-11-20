5e8036c0239edc9299b875e129889f097b6e17ce
Zackery Bayle <Zackery.bayle@gmail.com>
2019-11-20 11:50:07 -0700

    files commit

---
+++ Python/htmlGen/app.py
@@ -0,0 +1,21 @@
+class Html:
+    def __init__(self, content):
+        self.content = content
+
+    def render(self):
+        raise NotImplementedError('Subclass must implement render method')
+
+class Heading(Html):
+    def render(self):
+            return f'<h1>{self.content}</h1>'
+class Div(Html):
+    def render(self):
+            return f'<div>{self.content}</div>'
+tags = [
+    Div('some content'),
+    Heading('Some Big heading'),
+    Div('Another Div')
+]
+
+for tag in tags:
+    print(tag.render())

18e34f8bee90315e641845a5b5de4926a359a022
Zackery Bayle <Zackery.bayle@gmail.com>
2020-01-08 08:06:06 -0700

    error managment commit

--- Portpholio/container site/prettyPrice.js
+++ Portpholio/container site/prettyPrice.js
@@ -1,7 +1,10 @@
 const prettyPrice = (grossPrice, extension) => {
+
+    if (Number.isInteger(extension)) {
+        extension = extension * 0.01;
+    }
+
     return Math.floor(grossPrice) + extension;
 };
 
-
-
-console.log(prettyPrice(3.42, 0.99));
+console.log(prettyPrice(4.51, 99));

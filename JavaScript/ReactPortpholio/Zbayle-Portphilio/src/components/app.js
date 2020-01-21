c5b529e65adb9eff1b9c16ce60b3d98e890b1907
Zackery Bayle <Zackery.bayle@gmail.com>
2020-01-21 07:27:47 -0700

    NPM INIT FIX

---
+++ JavaScript/ReactPortpholio/Zbayle-Portphilio/src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+
+        
+        <h1>Zackery Bayle</h1>
+        <h3>A web Developer</h3>
+      </div>
+    );
+  }
+}
---
+++ JavaScript/ReactPortpholio/Zbayle-Portphilio/src/components/helpers/headerNav.js
@@ -0,0 +1,11 @@
+
+import React, { Component } from 'react';
+
+/**
+ * Returns a string element element with a footer and updating year
+ * @param {string} name
+ * @return {string}
+ */
+
+
+ 

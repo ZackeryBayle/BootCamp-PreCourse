fa78ef7f1268996e481a075cac7450f597031958
Zackery Bayle <Zackery.bayle@gmail.com>
2020-03-02 07:57:38 -0700

    state commit

--- JavaScript/Madlibs/Zackery-bayle-react-portfolio/src/components/app.js
+++ JavaScript/Madlibs/Zackery-bayle-react-portfolio/src/components/app.js
@@ -7,7 +7,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Zackery Bayle React Portfoli0</h1>
+        <h1>Zackery Bayle React Portfolio</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
 
         <PortfolioContainer />
--- JavaScript/Madlibs/Zackery-bayle-react-portfolio/src/components/portfolio/portfolio-container.js
+++ JavaScript/Madlibs/Zackery-bayle-react-portfolio/src/components/portfolio/portfolio-container.js
@@ -5,15 +5,32 @@ import PortfolioItem from "./portfolio-item";
 export default class PortfolioContainer extends Component {
     constructor() {
         super();
-        console.log("Portfolio Container: Render");
+        
+        this.state = {
+            pageTitle: "Welcome to my Portfolio",
+            data: [
+                {title: "Quip"},
+                {type: "jr dev"},
+                {title: "EventBright"},
+                {type: "sr dev"},
+                {title: "Ministry Safe"},
+                {type: "gen dev"}
+            ]
+        };
+    }
+    portfolioItems() {
+
+        return this.state.data.map(item => {
+        return <PortfolioItem title={item.title} type={item.type} />;
+        });
     }
 
     render() {
         return (
             <div>
-                <h2>Portfolio Container</h2>
+                <h2>{this.state.pageTitle}</h2>
 
-                <PortfolioItem />
+                { this.portfolioItems() }
             </div>
         );
     }

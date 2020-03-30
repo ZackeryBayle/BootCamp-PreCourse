70bd941c83195375067eb436685d5b45d5d479af
Zackery Bayle <Zackery.bayle@gmail.com>
2020-03-30 08:18:41 -0700

    Auth & login / out done

--- JavaScript/Madlibs/Zackery-bayle-react-portfolio/src/components/app.js
+++ JavaScript/Madlibs/Zackery-bayle-react-portfolio/src/components/app.js
@@ -5,6 +5,7 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
+import axios from 'axios';
 
 
 import NavigationContainer from "./navigation/navigation-container";
@@ -13,23 +14,110 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from './portfolio/portfolio-detail';
+import Auth from './pages/auth';
 import NoMatch from "./pages/no-match";
+// import { response } from 'express';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    };
+    this.handleSuccessfullLogin = this.handleSuccessfullLogin.bind(this);
+    this.handleUnsuccessfullLogin = this.handleUnsuccessfullLogin.bind(this);
+    this.handleSuccessLogout = this.handleSuccessLogout.bind(this);
+  }
   
+  handleSuccessfullLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    })
+  }
+  handleUnsuccessfullLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    })
+  }  
+  
+  handleSuccessLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    })
+  }
+
+  checkLoginStatus() {
+    return axios.get("https://api.devcamp.space/logged_in", {
+      withCredentials: true
+    }).then(response => {
+      const loggedIn = response.data.logged_in;
+      const loggedInStatus = this.state.loggedInStatus;
+
+      //if loggedIn and status LOGGED_IN => return data
+      //if loggedIn status NOT_LOGGED_IN => update state
+      //if not loggedIn and status LOGGED_IN => update state
+
+      if (loggedIn && loggedInStatus === "LOGGED_IN") {
+        return loggedIn;
+      }else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "LOGGED_IN"
+        });
+      }else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN"
+        });
+      }
+    })
+    .catch(error => {
+      console.log("ERROR", error);
+    });
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
+
+
+  authorizedPages() {
+    return [
+    ]
+  }
+
+
   render() {
     return (
       <div className='container'>
 
         <Router>
           <div>
-            <NavigationContainer />
+            <NavigationContainer 
+              loggedInStatus ={this.state.loggedInStatus}
+              handleSuccessLogout = {this.handleSuccessLogout}
+            />
+            <h2>{this.state.loggedInStatus}</h2>
             <Switch>
               <Route exact path="/" component={Home} />
+
+              <Route 
+                path="/auth"
+                render={props => (
+                    <Auth
+                      {...props}
+                      handleSuccessfullLogin = {this.handleSuccessfullLogin}
+                      handleUnsuccessfullLogin = {this.handleUnsuccessfullLogin}
+                    />
+                  )
+                }
+              />
+
+
+
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
+              {this.state.loggedInStatus === "LOGGED_IN" ? this.authorizedPages(): null}
               <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
               <Route component={NoMatch} />
             </Switch>
---
+++ JavaScript/Madlibs/Zackery-bayle-react-portfolio/src/components/auth/login.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react'; 
+import axios from 'axios';
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            errorText: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this); 
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+
+    
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+            errorText: ""
+        });
+    }
+
+
+    handleSubmit(event) {
+        axios
+          .post(
+            "https://api.devcamp.space/sessions",
+            {
+              client: {
+                email: this.state.email,
+                password: this.state.password
+              }
+            },
+            { withCredentials: true }
+          )
+          .then(response => {
+            console.log("response", response);
+            if (response.data.status === "created") {
+                // console.log("Logged in!!!")
+                this.props.handleSuccessfullAuth();
+            }else {
+                this.setState({
+                    errorText: "Wrong Email or Password"
+                });
+                this.props.handleUnsuccessfullAuth();
+            }
+          }).catch(error => {
+              console.log(error.description);
+              this.setState({
+                  errorText: error.description
+              });
+              this.props.handleUnsuccessfullAuth();
+          });
+    
+        event.preventDefault();
+      }
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>Login to access your dashboard</h1>
+
+                <div>
+                    {this.state.errorText}
+                </div>
+                
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="email" 
+                        name="email"
+                        placeholder="Your Email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+
+                    <input 
+                        type="password" 
+                        name="password"
+                        placeholder="Your Password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
+            </div>        
+        );
+    }
+}
